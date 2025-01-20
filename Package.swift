1fb443180536201379da433a73724d9670c5ce2d
spvagelis <spirou.vagelis@gmail.com>
2025-01-20 15:46:37 +0200

    first commit

--- Package.swift
+++ Package.swift
@@ -5,17 +5,25 @@ import PackageDescription
 
 let package = Package(
     name: "CombineSPMS",
+    platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "CombineSPMS",
             targets: ["CombineSPMS"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/spvagelis/sharedSPM.git",
+                 exact: "1.0.0"),
+        .package(url: "https://github.com/spvagelis/AntifraudSPM.git", exact: "1.0.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "CombineSPMS"),
+            name: "CombineSPMS",
+            dependencies: ["sharedSPM", "AntifraudSPM"]
+        ),
         .testTarget(
             name: "CombineSPMSTests",
             dependencies: ["CombineSPMS"]),
