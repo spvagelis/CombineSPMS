a9fa034a69b7ce42df1303073c93ac3aaf5c5aaf
spvagelis <spirou.vagelis@gmail.com>
2025-01-21 15:34:18 +0200

    Receive the AntifraudSPM version 1.0.2

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/spvagelis/sharedSPM.git",
                  exact: "1.0.1"),
-        .package(url: "https://github.com/spvagelis/AntifraudSPM.git", exact: "1.0.1")
+        .package(url: "https://github.com/spvagelis/AntifraudSPM.git", exact: "1.0.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
