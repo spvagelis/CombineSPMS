d6fbfed3657f2ce8f14050dda286feeaa7b85329
spvagelis <spirou.vagelis@gmail.com>
2025-01-21 14:35:21 +0200

    Upadeted the version of sharedSPM and AntifraudSPM from 1.0.0 to 1.0.1

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/spvagelis/sharedSPM.git",
-                 exact: "1.0.0"),
-        .package(url: "https://github.com/spvagelis/AntifraudSPM.git", exact: "1.0.0")
+                 exact: "1.0.1"),
+        .package(url: "https://github.com/spvagelis/AntifraudSPM.git", exact: "1.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
